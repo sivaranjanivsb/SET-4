92a1e6c5f5bc95dc00d4ebceb64d744313784617
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-03-20 10:50:00 +0000

    Update swap using bitwise operator.c

--- swap using bitwise operator.c
+++ swap using bitwise operator.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-	int no1,no2;
-	scanf("%d %d",&no1,&no2);
-	no1=no1^no2;
-	no2=no1^no2;
-	no1=no1^no2;
-	printf("%d %d",no1,no2);
+	int a1,a2;
+	scanf("%d %d",&a1,&a2);
+	a1=a1^a2;
+	a2=a1^a2;
+	a1=a1^a2;
+	printf("%d %d",a1,a2);
 	return 0;
 }
